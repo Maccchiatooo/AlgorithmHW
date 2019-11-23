2c18102bb91761b62b8acfa3ed4319a92b8fcbdf
macchiato <macchiato@macchiatodeMBP.fios-router.home>
2019-11-22 23:49:35 -0500

    update

--- HW4/HW4.cpp
+++ HW4/HW4.cpp
@@ -12,18 +12,120 @@ vertex_list * graph_part(int n, int k, vertex_list * edges[], int r){
     //vertex_list 1 *edges[1]
     //1->next[1]= first neighbor
 
-    vertex_list A[k];
-    for (int i=0;i<k;i++){
-        A[i]=*edges[i];
+    int *len;
+    for(int i=0; i<n; i++){
+        vertex_list Edges[i]= * edges[i];
+        len[i]=0;
+        while(Edges[i].next){
+            Edges[i].next++;
+            len[i]++;
+        }
     }
 
-    vertex_list B[n-k];
-    for (int i=0;i<n-k;i++){
-        B[i]=*edges[i+k];
+    vertex_list * A;
+    for (int i=0; i<k; i++){
+        A[i] = * edges[i];
     }
 
+    vertex_list * B;
+    for (int i=0; i<n-k; i++){
+        B[i] = * edges[i+k];
+    }
+    int *D;
+    int *inter;
+    int *exter;
+    for (int i=0; i<k; i++){
+        inter[i]=0;
+        exter[i]=0;
+        for (int jj=0; jj<len[i];jj++){
+            
+            for (int j=i+1; j<k; j++){
+                if (A[i].next[jj].v==A[j].v){
+                    g[i][j]=1;
+                    inter[i]++;
+                }
+            }
 
+            for (int ii=0; ii<n-k; ii++){
+
+                if (A[i].next[jj].v==B[ii].v){
+                    exter[i]++;
+                    g[i][ii]=1;
+                }
+        
+            }
+        
+        }
+        
+        D[i]=exter[i]-inter[i];
+
+    }
+
+    for (int i=k; i<n; i++){
+        inter[i]=0;
+        exter[i]=0;
+        for (int jj=0; jj<len[i];jj++){
+            
+            for (int j=i+1; j<n; j++){
+                if (A[i].next[jj].v==A[j].v){
+                    inter[i]++;
+                }
+            }
+
+            for (int ii=0; ii<k; ii++){
+
+                if (A[i].next[jj].v==B[ii].v){
+                    exter[i]++;
+                }
+        
+            }
+        
+        }
+
+        D[i]=exter[i]-inter[i];    
     
+    }
+
+
+    int ** g;
+
+    for (int i=0;i<k;i++){
+        for (int j=k;j<n;j++){
+            for(int jj=0;jj<len[i];jj++){
+                if (A[i].next[jj].v==B[j].v)
+                g[i][k]=D[i]-D[j]-2;
+            }
+            g[i][k]=D[i]-D[j];
+        }
+    }
+    int b=0;
+    int c=0;
+    int a=g[0][0];
+     for (int i=0;i<k;i++){
+        for (int j=k;j<n;j++){
+        
+
+        if (a<g[i][j]) {
+            a=g[i][j];
+            b=i;
+            c=j;
+        }
+        
+        }
+     }
+    int n=0; 
+    int G[n]=a;
+    
+    int Gm=0;
+    for (int i=0;i<n;i++){
+        Gm+=G[i]
+    }
+    A[b].v=0;
+    B[c].v=0;
+
+    while(Gm<0||Gm==0);
+
+
 };
 /*
 int main(){
